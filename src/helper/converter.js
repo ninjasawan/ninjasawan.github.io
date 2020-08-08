ef56ea5762f43c69e278dab9f013b5962805befc
Sawan Kumar <sawankumar@apollo247.org>
2020-08-08 18:16:03 +0530

    Max min values added

--- src/helper/converter.js
+++ src/helper/converter.js
@@ -7,3 +7,25 @@ export const convertToJson = (data, keys, delim = ",") =>
     );
     return [...initial, obj];
   }, []);
+
+export const getMaxMin = (data) => {
+  let maxOpen = data.sort((a, b) => b.open - a.open)[0],
+    minOpen = data.sort((a, b) => a.open - b.open)[0],
+    maxHigh = data.sort((a, b) => b.high - a.high)[0],
+    minHigh = data.sort((a, b) => a.high - b.high)[0],
+    maxLow = data.sort((a, b) => b.low - a.low)[0],
+    minLow = data.sort((a, b) => a.low - b.low)[0],
+    maxClosed = data.sort((a, b) => b.close - a.close)[0],
+    minClosed = data.sort((a, b) => a.close - b.close)[0];
+
+  return {
+    maxOpen,
+    minOpen,
+    maxHigh,
+    minHigh,
+    maxLow,
+    minLow,
+    maxClosed,
+    minClosed,
+  };
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { fetchWrapper, convertToJson } from "helper";
+import { fetchWrapper, convertToJson, getMaxMin } from "helper";
 import { makeStyles } from "@material-ui/core/styles";
 import { AppLoader, DataTable, AppContainer } from "components";
 import moment from "moment";
 import SyncIcon from "@material-ui/icons/Sync";
+import { Grid, Paper, Typography } from "@material-ui/core";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import purple from "@material-ui/core/colors/purple";
+import teal from "@material-ui/core/colors/teal";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
+  gridContainer: {
+    marginBottom: 20,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 600,
+    margin: "0 0 20px",
+  },
+  values: {
+    fontSize: 14,
+    lineHeight: 1.5,
+  },
+  upArrow: {
+    fontSize: 100,
+    color: purple[500],
+  },
+  downArrow: {
+    fontSize: 100,
+    color: teal[500],
+  },
 }));
 
 const columns = [
@@ -96,6 +127,7 @@ function Home() {
   const classes = useStyles();
   const [loading, setLoading] = useState(false);
   const [historicalData, setHistoricalData] = useState([]);
+  const [maxMin, setMaxMin] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
@@ -103,7 +135,9 @@ function Home() {
     fetchWrapper
       .get(`${process.env.REACT_APP_HISTORY_ENDPOINT}?interval=${currentPage}`)
       .then((response) => {
-        setHistoricalData(convertToJson(response, keys));
+        const data = convertToJson(response, keys);
+        setHistoricalData(data);
+        setMaxMin(getMaxMin(data));
         setLoading(false);
       })
       .catch((error) => {
@@ -115,6 +149,75 @@ function Home() {
   return (
     <section className={classes.root}>
       <AppContainer>
+        {maxMin && (
+          <Grid container className={classes.gridContainer} spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>
+                <div className={classes.left}>
+                  <Typography variant="h3" className={classes.title}>
+                    Maximum
+                  </Typography>
+                  <Typography variant="h3" className={classes.values}>
+                    {`Open:- ₹${maxMin.maxOpen.open} on ${moment(
+                      Number(maxMin.maxOpen.timestamp)
+                    ).format("MMM DD, YYYY")}`}
+                  </Typography>
+                  <Typography variant="h3" className={classes.values}>
+                    {`High:- ₹${maxMin.maxHigh.high} on ${moment(
+                      Number(maxMin.maxHigh.timestamp)
+                    ).format("MMM DD, YYYY")}`}
+                  </Typography>
+                  <Typography variant="h3" className={classes.values}>
+                    {`Low:- ₹${maxMin.maxLow.low} on ${moment(
+                      Number(maxMin.maxLow.timestamp)
+                    ).format("MMM DD, YYYY")}`}
+                  </Typography>
+                  <Typography variant="h3" className={classes.values}>
+                    {`Close:- ₹${maxMin.maxClosed.open} on ${moment(
+                      Number(maxMin.maxClosed.timestamp)
+                    ).format("MMM DD, YYYY")}`}
+                  </Typography>
+                </div>
+                <div className={classes.right}>
+                  <ArrowUpwardIcon className={classes.upArrow} />
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>
+                <div className={classes.left}>
+                  <Typography variant="h3" className={classes.title}>
+                    Minimum
+                  </Typography>
+                  <Typography variant="h3" className={classes.values}>
+                    {`Open:- ₹${maxMin.minOpen.open} on ${moment(
+                      Number(maxMin.minOpen.timestamp)
+                    ).format("MMM DD, YYYY")}`}
+                  </Typography>
+                  <Typography variant="h3" className={classes.values}>
+                    {`High:- ₹${maxMin.minHigh.high} on ${moment(
+                      Number(maxMin.minHigh.timestamp)
+                    ).format("MMM DD, YYYY")}`}
+                  </Typography>
+                  <Typography variant="h3" className={classes.values}>
+                    {`Low:- ₹${maxMin.minLow.low} on ${moment(
+                      Number(maxMin.minLow.timestamp)
+                    ).format("MMM DD, YYYY")}`}
+                  </Typography>
+                  <Typography variant="h3" className={classes.values}>
+                    {`Close:- ₹${maxMin.minClosed.open} on ${moment(
+                      Number(maxMin.minClosed.timestamp)
+                    ).format("MMM DD, YYYY")}`}
+                  </Typography>
+                </div>
+                <div className={classes.right}>
+                  <ArrowDownwardIcon className={classes.downArrow} />
+                </div>
+              </Paper>
+            </Grid>
+          </Grid>
+        )}
+
         <DataTable
           title="Historical Records"
           columns={columns}
@@ -129,7 +232,7 @@ function Home() {
               },
             },
           ]}
-          options={{ maxBodyHeight: "calc(100vh - 250px)" }}
+          options={{ maxBodyHeight: "calc(100vh - 430px)" }}
         />
       </AppContainer>
 
